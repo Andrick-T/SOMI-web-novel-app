c6507ef04a85114e4da72ac441b191974d3cabf7
Andrick-T <andricktchoukeu@gmail.com>
2026-09-24 16:25:45 +0200

    feat(payments): register payment history pages

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ const BookDetailPage = lazy(() => import("./pages/BookDetailPage"));
 const ReaderPage = lazy(() => import("./pages/ReaderPage"));
 const LibraryPage = lazy(() => import("./pages/LibraryPage"));
 const WalletPage = lazy(() => import("./pages/WalletPage"));
-const PaymentReturnPage = lazy(() => import("./pages/PaymentReturnPage"));
+const PaymentReturnPage = lazy(() => import("./pages/PaymentReturnPage"));\nconst PaymentHistoryPage = lazy(() => import("./pages/PaymentHistoryPage"));
 const ProfilePage = lazy(() => import("./pages/ProfilePage"));
 const AuthPage = lazy(() => import("./pages/AuthPage"));
 
